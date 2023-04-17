17f5eadd12c65b9da52430729a02af1da4387dde
Lawal Taofik <layomide81@gmail.com>
2023-04-17 21:49:22 +0000

    fix var

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-	let task = false;
-	let task2 = true;
-    return [task, task2]
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
 }
+
