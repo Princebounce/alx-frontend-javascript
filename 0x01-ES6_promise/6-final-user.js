497b1624764c2ec9a0a40bea88057df9a6483dec
Lawal Taofik <layomide81@gmail.com>
2023-04-20 00:18:23 +0100

    handle all promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
 
-  return Promise.allSettled([userPromise, photoPromise])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })))
-    .catch(() => [{ status: 'rejected', value: 'Signup system offline' }]);
+  return Promise.allSettled(promises).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  }))).catch(() => [{
+    status: 'rejected',
+    value: 'Signup system offline',
+  }]);
 }
